847a88f5edf8caca544517d5e88140387f27670c
seagate25 <sigidh.budi@gmail.com>
2020-09-16 20:38:58 +0700

    sales module & default view

--- application/modules/sales/controllers/Sales.php
+++ application/modules/sales/controllers/Sales.php
@@ -3,10 +3,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sales extends CI_Controller {
-
+    
+    public function __construct()
+    {
+        parent::__construct();
+        logged_in();
+        $this->load->model('Sales_model', 'sales_model');
+        
+    }
+    
     public function index()
     {
-        
+        $data['title']      = "Penjualan (Kasir)";
+        $data['content']    = "index";
+        $this->load->view('default', $data);
     }
 
 }
--- application/views/default.php
+++ application/views/default.php
@@ -1644,7 +1644,7 @@
 		<!-- end::Scrolltop -->
 
 		<!-- begin::Sticky Toolbar -->
-		<ul class="kt-sticky-toolbar" style="margin-top: 30px;">
+		<!-- <ul class="kt-sticky-toolbar" style="margin-top: 30px;">
 			<li class="kt-sticky-toolbar__item kt-sticky-toolbar__item--success" id="kt_demo_panel_toggle" data-toggle="kt-tooltip" title="Check out more demos" data-placement="right">
 				<a href="#" class=""><i class="flaticon2-drop"></i></a>
 			</li>
@@ -1657,7 +1657,7 @@
 			<li class="kt-sticky-toolbar__item kt-sticky-toolbar__item--danger" id="kt_sticky_toolbar_chat_toggler" data-toggle="kt-tooltip" title="Chat Example" data-placement="left">
 				<a href="#" data-toggle="modal" data-target="#kt_chat_modal"><i class="flaticon2-chat-1"></i></a>
 			</li>
-		</ul>
+		</ul> -->
 
 		<!-- end::Sticky Toolbar -->
 
